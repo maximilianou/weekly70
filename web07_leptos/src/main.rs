b97fae9533df3adfa760c2e6a59b40c9b4b0471b
Maximiliano Usich <maximilianou@gmail.com>
2024-09-21 09:56:48 +0200

    feat: fragment in rust web

--- web07_leptos/src/main.rs
+++ web07_leptos/src/main.rs
@@ -1,4 +1,4 @@
 use leptos::*;
 fn main() {
-    mount_to_body( || view! { <p>"Hi, here we are!"</p> } )
+    mount_to_body( || view! { <><p>"Hi, here we are!"</p><ul><li>"Rust"</li><li>"Web"</li></ul></> } )
 }

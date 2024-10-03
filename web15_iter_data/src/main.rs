1ce9697ee2c392eeb30277b55eb719d6a2f2b045
Maximiliano Usich <maximilianou@gmail.com>
2024-10-03 18:31:39 +0200

    feat: add databaseentry

--- web15_iter_data/src/main.rs
+++ web15_iter_data/src/main.rs
@@ -9,11 +9,40 @@ struct DatabaseEntry{
 #[component]
 fn App() -> impl IntoView {
 
+  let (data, set_data) = create_signal(vec![
+    DatabaseEntry {
+        key: "Une".to_string(),
+        value: 10,
+    },
+    DatabaseEntry {
+        key: "Due".to_string(),
+        value: 20,
+    },
+    DatabaseEntry {
+        key: "Tre".to_string(),
+        value: 30,
+    },
 
+  ]);
 
-    view! {
-
-    }
+  view! {
+    <button on:click=move |_| {
+      set_data.update(|data| {
+        for row in data {
+            row.value += 2;
+        }
+      });
+      logging::log!("{:?}", data.get());
+    }>
+      "Update Values"
+    </button>
+    <For each=data
+         key=|state| ( state.key.clone(), state.value )
+         let:child
+         >
+         <p>{child.value}</p>
+    </For>
+  }
 }
 
 fn main() {

7f80074fc41c7e0c215da817ca2f000ea2d3636c
Maximiliano Usich <maximilianou@gmail.com>
2024-10-05 10:22:10 +0200

    feat: reactivity over RwSignal and create_rw_signal()

--- web15_iter_data/src/main.rs
+++ web15_iter_data/src/main.rs
@@ -1,9 +1,15 @@
 use leptos::*;
-
+/*
 #[derive(Debug, Clone)]
 struct DatabaseEntry{
     key: String,
     value: i32,
+}
+*/
+#[derive(Debug, Clone)]
+struct DatabaseEntry{
+    key: String,
+    value: RwSignal<i32>,
 }
 
 #[component]
@@ -12,32 +18,40 @@ fn App() -> impl IntoView {
   let (data, set_data) = create_signal(vec![
     DatabaseEntry {
         key: "Une".to_string(),
-        value: 10,
+        value: create_rw_signal(10),
     },
     DatabaseEntry {
         key: "Due".to_string(),
-        value: 20,
+        value: create_rw_signal(20),
     },
     DatabaseEntry {
         key: "Tre".to_string(),
-        value: 30,
+        value: create_rw_signal(30),
     },
 
   ]);
 
   view! {
     <button on:click=move |_| {
+        /*
       set_data.update(|data| {
         for row in data {
             row.value += 2;
         }
       });
+            */
+       data.with(|data|{
+         for row in data {
+            row.value.update(|value| *value *= 2);
+         }
+       });
       logging::log!("{:?}", data.get());
     }>
       "Update Values"
     </button>
     <For each=data
-         key=|state| ( state.key.clone(), state.value )
+/*       key=|state| ( state.key.clone(), state.value ) */
+         key=|state| state.key.clone()
          let:child
          >
          <p>{child.value}</p>

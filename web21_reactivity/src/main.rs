8d470f92f72cd1bb72931cb2f0e03d226b3bbd43
Maximiliano Usich <maximilianou@gmail.com>
2024-10-17 07:00:41 +0200

    wip: adding reactividy

--- web21_reactivity/src/main.rs
+++ web21_reactivity/src/main.rs
@@ -15,8 +15,78 @@ fn App() -> impl IntoView {
   }
 }
 
+#[component]
+fn CreateAnEffect() -> impl IntoView {
+  let (first, set_first) = create_signal(String::new());
+  let (last, set_last)   = create_signal(String::new());
+  let (use_last, set_use_last) = create_signal(true);
+  create_effect( move |_| {
+    log(if use_last() {
+      with!(|first, last| format("{first} {last}"))
+    } else {
+      first()
+    })
+  });
+  view! {
+    <h1>
+      <code>"create effect"</code>
+      "Version"
+    </h1>
+    <form>
+      <label>
+        "First Name"
+        <input type="text" name="first" prop:value=first
+          on:change=move |ev| set_first(event_target_value(&ev))  />
+      </label>
+      <label>
+        "Last Name"
+        <input type="text" name="last" prop:value=last 
+          on:change=move |ev| set_last(event_target_value(&ev))  />
+      </label>
+      <label>
+        "Show Last Name"
+        <input type="checkbox" name="use_last" prop:checked=use_last
+          on:change=move |ev| set_use_last(event_target_checked(&ev)) />
+      </label>
+    </form>
+  }
+}
+
+#[compnent]
+fn ManualVersion() -> impl IntoView {
+    let first    = create_node_ref::<Input>();
+    let last     = create_node_ref::<Input>();
+    let use_last = create_node_ref::<Input>();
+    let mut prev_name = String::new();
+    let on_change = move |_| {
+      log("      listener");
+      let first    = first.get().unwrap();
+      let last     = last.get().unwrap();
+      let use_last = use_last.get().unwrap();
+      let this_one = if use_last.checked() {
+        format!("{} {}", first.value(), last.value())
+      } else {
+        first.value()
+      };
+      if this_one != prev_name {
+        log(&this_one);
+        prev_name = this_one;
+      }
+    };
+    view! {
+      <h1>
+        "Manual Version"
+      </h1>
+      <form on:change=on_change>
+        <label>"First Name" 
+        <input />
+        </label>
+      </form>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+  leptos::mount_to_body(App);
 }
 
 

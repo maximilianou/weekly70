0e72efebeaee0643064211eb90bc3ed8983502f5
Maximiliano Usich <maximilianou@gmail.com>
2024-10-26 17:36:59 +0200

    feat: router leptos

--- web27_routes/src/main.rs
+++ web27_routes/src/main.rs
@@ -1,8 +1,76 @@
 use leptos::*;
+use leptos_router::*;
+
 #[component]
 fn App() -> impl IntoView {
   view! {
-    
+    <Router>
+      <h1>"Contact App"</h1>
+      <nav>
+        <h2>"Navigation"</h2>
+        <a href="/">"Home"</a>
+        <a href="/contacts">"Contacts"</a>
+      </nav>
+      <main>
+        <Routes>
+          <Route path="/contacts" view=ContactList >
+            <Route path=":id" view=ContactInfo >
+              <Route path="" view=|| view! {
+                <div class="tab">
+                "(Contact Info)"
+                </div>
+              }/>
+              <Route path="conversatios" view=|| view! {
+                <div class="tab">
+                "(Conversations)"
+                </div>
+              }/>
+            </Route>
+            <Route path="" view=|| view! {
+              <div class="select-user">
+                "Select a user to view contacts info."
+              </div>
+            } />            
+          </Route>
+        </Routes>
+      </main>
+    </Router>    
+  }
+}
+
+#[component]
+fn ContactList() -> impl IntoView {
+  view! {
+    <div class="contact-list">
+      <div class="contact-list-contacts">
+        <h3>"Contacts"</h3>
+        <A href="juan">"Juan"</A>
+        <A href="paolo">"Paolo"</A>
+        <A href="luca">"Luca"</A>
+      </div>
+      <Outlet />
+    </div>
+  }
+}
+#[component]
+fn ContactInfo() -> impl IntoView {
+  let params = use_params_map();
+  let id = move || params.with(|params| params.get("id").cloned().unwrap_or_default());
+  let name = move || match id().as_str() {
+    "juan" => "Juan",
+    "paolo" => "Paolo",
+    "luca" => "Luca",
+    _ => "User not found",
+  };
+  view! {
+    <div class="contact-info">
+      <h4>{name}</h4>
+      <div class="tabs">
+        <A href="" exact=true >"Contact Info"</A>
+        <A href="conversations"  >"Conversations"</A>
+      </div>
+      <Outlet />
+    </div>
   }
 }
 fn main() {

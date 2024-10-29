c9393969b8c82722e748e4afc44fd62e90e482f9
Maximiliano Usich <maximilianou@gmail.com>
2024-10-29 06:58:57 +0100

    feat: filling the form

--- web30_form/src/main.rs
+++ web30_form/src/main.rs
@@ -1,9 +1,43 @@
 use leptos::*;
+use leptos_router::*;
 
 #[component]
 fn App() -> impl IntoView {
     view! {
-
+        <Router>
+            <h1><code>" <Form /> "</code></h1>
+            <main>
+              <Routes>
+               <Route path="" view=FormExample />
+              </Routes>
+            </main>
+        </Router>
+    }
+}
+#[component]
+fn FormExample() -> impl IntoView {
+    let query  = use_query_map();
+    let name   = move || query().get("name").cloned().unwrap_or_default();
+    let number = move || query().get("number").cloned().unwrap_or_default();
+    let select = move || query().get("select").cloned().unwrap_or_default();
+    view! {
+        <table>
+          <tr>
+            <td><code>"name"</code></td>
+            <td>{name}</td>
+          </tr>
+          <tr>
+            <td><code>"number"</code></td>
+            <td>{number}</td>
+          </tr>
+          <tr>
+            <td><code>"select"</code></td>
+            <td>{select}</td>
+          </tr>
+        </table>
+        <h2>"Manual Submission"</h2>
+        <Form method="GET" action="" >
+        </Form>
     }
 }
 fn main() {

b8e56843928fb9b00db6f2d06f7410cb9a78cade
Maximiliano Usich <maximilianou@gmail.com>
2024-11-17 16:49:49 +0100

    feat: add database connection pool

--- web46-docker/bookstore/src/main.rs
+++ web46-docker/bookstore/src/main.rs
@@ -1,12 +1,16 @@
 use std::error::Error;
-use sqlx::Connection;
+//use sqlx::Connection;
 use sqlx::Row;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let url = "postgres://demo:demo@localhost:5432/demo";
-    let mut conn = sqlx::postgres::PgConnection::connect(url).await?;
+//    let mut conn = sqlx::postgres::PgConnection::connect(url).await?;
+    let pool = sqlx::postgres::PgPool::connect(url).await?;
+    //let res = sqlx::query("SELECT 1 + 1 as sum")
+    //    .fetch_one(&mut conn)
+    //    .await?;
     let res = sqlx::query("SELECT 1 + 1 as sum")
-        .fetch_one(&mut conn)
+        .fetch_one(&pool)
         .await?;
     let sum: i32 = res.get("sum");
     println!(" 1 + 1 = {} ", sum);
